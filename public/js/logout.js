30af674a5b78052ad308c74e351957500e9964aa
Matthew Moore <mattholtmoore@gmail.com>
2022-01-12 13:47:10 -0500

    fixed logout route

--- public/js/logout.js
+++ public/js/logout.js
@@ -1,12 +1,12 @@
 const logout = async () => {
-  
+
   const res = await fetch('/api/users/logout', {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
   });
 
   if (res.ok) {
-    document.location.replace('/login');
+    document.location.replace('/');
   } else {
     alert(res.statusText);
   }
